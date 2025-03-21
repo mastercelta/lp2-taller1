458974709c47be5a23fe927223087bd5cac2f553
Mastercelta <juancamilo@bees.com.co>
2025-03-20 20:58:54 -0500

    Taller 1 cambio

---
+++ datos.py
@@ -0,0 +1,9 @@
+productos = [
+    {"nombre": "Celular", "precio": 1250},
+    {"nombre": "Tablet", "precio": 800},
+    {"nombre": "Computador", "precio": 2500},
+    {"nombre": "Consola", "precio": 1200},
+    {"nombre": "Lapiz", "precio": 5},
+    {"nombre": "Borrador", "precio": 1.2},
+    {"nombre": "Sacapuntas", "precio": 10},
+]
